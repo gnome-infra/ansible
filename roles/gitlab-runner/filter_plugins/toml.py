90b61c6431e39b6f2febc6380d173cfaf002c05f
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2023-07-22 10:03:52 +0200

    Rewrite gitlab-runner role

---
+++ roles/gitlab-runner/filter_plugins/toml.py
@@ -0,0 +1,41 @@
+# (c) 2017, Matt Martz <matt@sivel.net>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# Borrowed from https://github.com/sivel/toiletwater/blob/master/plugins/filter/toml.py
+
+# Make coding more python3-ish
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+from ansible.errors import AnsibleFilterError, AnsibleRuntimeError
+from ansible.module_utils._text import to_text
+from ansible.module_utils.common._collections_compat import MutableMapping
+from ansible.module_utils.six import string_types
+
+from ansible.plugins.inventory.toml import toml_dumps
+try:
+    from ansible.plugins.inventory.toml import toml_loads
+except ImportError:
+    def toml_loads(v):
+        raise AnsibleRuntimeError(
+            'The python "toml" or "tomli" library is required.'
+        )
+
+
+def from_toml(o):
+    if not isinstance(o, string_types):
+        raise AnsibleFilterError('from_toml requires a string, got %s' % type(o))
+    return toml_loads(to_text(o, errors='surrogate_or_strict'))
+
+
+def to_toml(o):
+    if not isinstance(o, MutableMapping):
+        raise AnsibleFilterError('to_toml requires a dict, got %s' % type(o))
+    return to_text(toml_dumps(o), errors='surrogate_or_strict')
+
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'to_toml': to_toml,
+            'from_toml': from_toml
+        }
