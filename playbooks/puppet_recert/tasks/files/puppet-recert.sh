393b04436c7df7a98e1956d633c0c994f4827f03
Andrea Veri <averi@redhat.com>
2024-10-23 16:20:33 -0400

    Deprecated

--- playbooks/puppet_recert/tasks/files/puppet-recert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-service puppet stop
-
-find /var/lib/puppet/ssl -type f -exec rm -f "{}" \;
-
-puppet agent --test --server puppetmaster.gnome.org --ca_server puppetmaster01.gnome.org
-
-service puppet restart
